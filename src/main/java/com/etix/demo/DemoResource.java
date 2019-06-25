0b314254eb524de0581d35d8bf7a00bcb8b5d559
Jinlong Xiong <jinlong.xiong@etix.com>
2019-06-25 17:24:49 +0800

    fix the issue that the parameter could be null

--- src/main/java/com/etix/demo/DemoResource.java
+++ src/main/java/com/etix/demo/DemoResource.java
@@ -4,12 +4,16 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@Produces(MediaType.TEXT_HTML)
 @Path("/demo/")
 public class DemoResource {
 
     @GET
     @Path("/hello")
     public Response hello(@QueryParam("name") String name){
+        if (name == null || name.length() == 0) {
+            name = "demo";
+        }
         return Response.ok("Hello, " + name).build();
     }
 }
