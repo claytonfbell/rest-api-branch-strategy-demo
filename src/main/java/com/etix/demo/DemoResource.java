0c42fa0e55dd6a7db03914352f3b4a631db7fd7a
Clay Bell <clay.bell@etix.com>
2019-06-26 13:27:41 -0700

    DEV-9999 Added new currentTime endpoint

--- src/main/java/com/etix/demo/DemoResource.java
+++ src/main/java/com/etix/demo/DemoResource.java
@@ -3,7 +3,11 @@ package com.etix.demo;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Produces(MediaType.TEXT_HTML)
 @Path("/demo")
@@ -16,5 +20,16 @@ public class DemoResource {
             name = "world";
         }
         return Response.ok("Hello, <b>" + name + "</b>. Now it is " + LocalDate.now() + "<br/> From: <b>" + from + "</b>").build();
+    }
+
+    @GET
+    @Path("/currentTime")
+    public Response currentTime() {
+
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        df.setTimeZone(tz);
+        String nowAsISO = df.format(new Date());
+        return Response.ok(nowAsISO).build();
     }
 }
